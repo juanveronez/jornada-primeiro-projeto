bece43431c38384467539a12374c9dc85d5d91af
Juan Veronez <juan.monteirov@gmail.com>
2025-01-21 09:49:16 -0300

    adicionando explicacao sobre tipos primitivos basicos e typecast entre esses tipos

---
+++ aula_1/2_primitive_types.py
@@ -0,0 +1,12 @@
+# tipos primitivos
+numero = 3 # int
+numero_decimal = 3.14 # float
+texto = 'Pi' # str
+eh_pi = True # bool
+
+# type cast
+print(float(numero)) # 3.0
+print(int(numero_decimal)) # 3 (pega a parte inteira)
+print(bool(texto)) # True
+print(bool('')) # False
+print(int(eh_pi)) # 1
