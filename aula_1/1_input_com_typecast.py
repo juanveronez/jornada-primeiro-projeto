a1574670f3a5a8827e5a2dda7ff133dcd60c1a34
Juan Veronez <juan.monteirov@gmail.com>
2025-01-21 09:48:41 -0300

    usando typecast para mudar o tipo para int e conseguir somar dois numeros inputados pelo usuario e printar

---
+++ aula_1/1_input_com_typecast.py
@@ -0,0 +1,6 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+val_1 = int(input('Valor 1: ')) # o tipo padrao de um input eh str
+val_2 = int(input('Valor 2: ')) # int eh um cast para o tipo int
+
+
+print(val_1 + val_2)
